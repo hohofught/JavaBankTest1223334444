5031126f4df2a8c258af3ba8efd1c449b3708f2e
hohofught <108419818+hohofught@users.noreply.github.com>
2025-12-15 23:48:14 +0900

    Delete bank directory

--- bank/src/bank/AccountOperationInterface.java
+++
@@ -1,9 +0,0 @@
-package bank;
-
-public interface AccountOperationInterface {
-	 public String getAccountNo();
-	 public void deposit(double amount);
-	 public void withdraw(double amount);
-	 public void transfer(Account yours, double amount);
-	 public double getBalance();	 
-}
